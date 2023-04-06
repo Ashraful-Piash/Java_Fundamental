867b5fb297f6f07aaaa3e48b04cc7178bce6345c
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-04-06 15:23:20 +0600

    Updated

--- src/com/piash/StringPractice.java
+++ src/com/piash/StringPractice.java
@@ -5,6 +5,8 @@ public class StringPractice {
         String s1 = "Hello";
         String s2 = "Hello";
         String s3 = new String("Hello");
+        String s4 = "I love programming";
+        String s5 = s4.replace("programming", "java");
 
         if(s1 == s3){
             System.out.println("equal");
@@ -17,5 +19,7 @@ public class StringPractice {
         System.out.println(s1.contains("Hello"));
         System.out.println(s1.toLowerCase());
         System.out.println(s2.toUpperCase());
+        System.out.println(s4);
+        System.out.println(s5);
     }
 }
