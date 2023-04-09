83ee8c99edfb820e3c90ac353fc645d935e58602
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-04-09 11:27:28 +0600

    Substring created

---
+++ src/com/piash/SubString.java
@@ -0,0 +1,11 @@
+package com.piash;
+
+public class SubString {
+    public static void main(String[] args) {
+        String s = "Hello Worlds";
+        String s2 = s.substring(5);
+        String s3 = s.substring(5,8);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
